b9db882fe602f69a9cbfaea95aa07fd69a7ced93
juniorjmm <josemjunior17@gmail.com>
2021-11-22 14:55:47 -0400

    movido clientside functions para dentro do escopo da func principal

--- assets/client.esnext.js
+++ assets/client.esnext.js
@@ -1,52 +1,55 @@
-async function getPosts(postType, postCategory, postTag, postsQuantity) {
-	const postsUrl = ufrGlobals.siteUrl + `/wp-json/wp/v2/posts?_embed=&_locale=user&per_page=${postsQuantity}`
+function ufrSetUpSliders(params) {
+	const { autoplay, height, sliderID } = params;
 
-	switch (postType) {
-		case 'most-recent':
-			return (await fetch(postsUrl)).json();
+	async function getPosts(postType, postCategory, postTag, postsQuantity) {
+		const postsUrl = ufrGlobals.siteUrl + `/wp-json/wp/v2/posts?_embed=&_locale=user&per_page=${postsQuantity}`
 
-		case 'most-seen':
-			return (await fetch(ufrGlobals.siteUrl + `/wp-json/ufr/most-seen-posts?quantity=${postsQuantity}`)).json();
+		switch (postType) {
+			case 'most-recent':
+				return (await fetch(postsUrl)).json();
 
-		case 'category':
-			return (await fetch(postsUrl + `&categories=${postCategory}`)).json();
+			case 'most-seen':
+				return (await fetch(ufrGlobals.siteUrl + `/wp-json/ufr/most-seen-posts?quantity=${postsQuantity}`)).json();
 
-		case 'tag':
-			return (await fetch(postsUrl + `&tags=${postTag}`)).json();
+			case 'category':
+				return (await fetch(postsUrl + `&categories=${postCategory}`)).json();
+
+			case 'tag':
+				return (await fetch(postsUrl + `&tags=${postTag}`)).json();
+		}
 	}
-}
 
-async function holdRenderForPosts(params) {
-	const { usePosts, useLegends, postType, postCategory, sliderID, postsQuantity, duration, postTag } = params;
-	const ufrLoadPosts = new Event('ufrLoadPosts');
+	async function holdRenderForPosts(params) {
+		const { usePosts, useLegends, postType, postCategory, sliderID, postsQuantity, duration, postTag } = params;
+		const ufrLoadPosts = new Event('ufrLoadPosts');
 
-	if (!usePosts) return window.dispatchEvent(ufrLoadPosts);
+		if (!usePosts) return window.dispatchEvent(ufrLoadPosts);
 
-	const mainSlider = document.getElementById(sliderID);
-	const thumbnailSlider = document.getElementById(`${sliderID}-thumbnail`);
-	const mainList = mainSlider.querySelector('.splide__list');
-	const thumbnailList = thumbnailSlider.querySelector('.splide__list');
+		const mainSlider = document.getElementById(sliderID);
+		const thumbnailSlider = document.getElementById(`${sliderID}-thumbnail`);
+		const mainList = mainSlider.querySelector('.splide__list');
+		const thumbnailList = thumbnailSlider.querySelector('.splide__list');
 
-	// Loader
+		// Loader
 
-	const posts = await getPosts(postType, postCategory, postTag, postsQuantity);
+		const posts = await getPosts(postType, postCategory, postTag, postsQuantity);
 
-	if (posts) {
-		posts.forEach(({ link, title, _embedded, thumbnail }) => {
-			let img = ufrGlobals.themeUrl + '/assets/img/logo/ufr-bg.png';
-			let imgAlt = '';
+		if (posts) {
+			posts.forEach(({ link, title, _embedded, thumbnail }) => {
+				let img = ufrGlobals.themeUrl + '/assets/img/logo/ufr-bg.png';
+				let imgAlt = '';
 
-			const embeddedImgAltTxt = _embedded ? _embedded['wp:featuredmedia']?.[0]?.alt_text: undefined;
-			const embeddedImg = _embedded ? _embedded['wp:featuredmedia']?.[0]?.source_url : undefined;
+				const embeddedImgAltTxt = _embedded ? _embedded['wp:featuredmedia']?.[0]?.alt_text: undefined;
+				const embeddedImg = _embedded ? _embedded['wp:featuredmedia']?.[0]?.source_url : undefined;
 
-			if (embeddedImg) img = embeddedImg;
-			if (embeddedImgAltTxt) imgAlt = embeddedImgAltTxt;
-			if (thumbnail) img = thumbnail;
-			if (!(postType === 'most-seen')) title = title.rendered;
+				if (embeddedImg) img = embeddedImg;
+				if (embeddedImgAltTxt) imgAlt = embeddedImgAltTxt;
+				if (thumbnail) img = thumbnail;
+				if (!(postType === 'most-seen')) title = title.rendered;
 
-			const legend = useLegends ? `<div class="description">${title}</div>` : '';
+				const legend = useLegends ? `<div class="description">${title}</div>` : '';
 
-			mainList.innerHTML += `
+				mainList.innerHTML += `
 				<li class="splide__slide"
 					data-splide-interval="${duration * 1000}"
 					style="cursor: pointer;"
@@ -59,19 +62,16 @@ async function holdRenderForPosts(params) {
 				</li>
 			`;
 
-			thumbnailList.innerHTML += `
+				thumbnailList.innerHTML += `
 				<li class="splide__slide">
 					<img src="${img}" alt="${imgAlt}" />
 				</li>
 			`;
-		})
+			})
+		}
+
+		window.dispatchEvent(ufrLoadPosts);
 	}
-
-	window.dispatchEvent(ufrLoadPosts);
-}
-
-function ufrSetUpSliders(params) {
-	const { autoplay, height, sliderID } = params;
 
 	window.addEventListener('ufrLoadPosts', function() {
 		const main = document.getElementById(sliderID);
