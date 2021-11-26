81bf63ef7729684ae9cb5593d04689ff3905a9fd
juniorjmm <josemjunior17@gmail.com>
2021-11-26 14:54:36 -0400

    pack script

--- pack.sh
+++ pack.sh
@@ -21,6 +21,6 @@ then
 	rm ./wp-idg-ufr__block-$blockname.zip
 fi
 
-zip wp-idg-ufr__block-$blockname.zip ./build ./assets/client-$blockname.js ./$blockname.php -r &&
+zip wp-idg-ufr__block-$blockname.zip ./build ./assets/client-$blockname.js ./wp-idg-ufr__block-$blockname.php -r &&
 
 echo "Done. Packed file: wp-idg-ufr__block-$blockname.zip"
